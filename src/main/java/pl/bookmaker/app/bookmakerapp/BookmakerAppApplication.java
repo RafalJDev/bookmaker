6d9f1a866ab950e47da93ec9f3639163af41bc04
rafaljaszczynski <jaszczynski.rafal>
2019-08-05 21:35:55 +0200

    First commit

---
+++ src/main/java/pl/bookmaker/app/bookmakerapp/BookmakerAppApplication.java
@@ -0,0 +1,13 @@
+package pl.bookmaker.app.bookmakerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmakerAppApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BookmakerAppApplication.class, args);
+    }
+    
+}
